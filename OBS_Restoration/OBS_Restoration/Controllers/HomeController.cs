6cfd8abf4e4a659aa38a67e536343e23912af865
Yevhen Toderash <yevhen.toderash@additiv.ch>
2019-11-11 11:56:28 +0200

    Default init

---
+++ OBS_Restoration/OBS_Restoration/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace OBS_Restoration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
