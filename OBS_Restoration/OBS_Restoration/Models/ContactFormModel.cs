492046f9a774765ec8698c81c3604038f1f228f4
Yevhen Toderash <yevhen.toderash@additiv.ch>
2019-11-24 23:28:31 +0200

    Updated HomeController with test methods

---
+++ OBS_Restoration/OBS_Restoration/Models/ContactFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OBS_Restoration.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
