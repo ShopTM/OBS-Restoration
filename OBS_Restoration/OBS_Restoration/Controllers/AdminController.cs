59c465d3d5bb8e015873e4ae0c5e19b8e489b87f
Yevhen Toderash <yevhen.toderash@additiv.ch>
2020-01-05 11:28:24 +0200

    Update admin navigation

---
+++ OBS_Restoration/OBS_Restoration/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OBS_Restoration.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
