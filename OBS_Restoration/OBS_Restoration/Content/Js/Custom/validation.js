9bb600a46abb721bbaffb8c78b1b804a7d331609
Inna <infarmationusa@gmail.com>
2020-02-08 22:13:43 +0200

    validation files input

--- OBS_Restoration/OBS_Restoration/Content/Js/Custom/validation.js
+++ OBS_Restoration/OBS_Restoration/Content/Js/Custom/validation.js
@@ -1,83 +1,71 @@
 ﻿$(function () {
-    const validationMassegeTemplate = "Please! Enter a valid ";
-    const emailValidationMassege = validationMassegeTemplate + "email address";
-    const phoneValidationMassege = validationMassegeTemplate + "phone number";
-    const fileValidationMassege = "Only file with the following extensions are allowed: jpg, jpeg, png, gif, doc, pdf, docx, txt";
-    const fileSizeValidationMassege = "Sorry! Maximum upload file size: 25 MB. Your file size is:"
-    let email = $("form input[type=email]");
-    let phone = $("form input[type=tel]");
-    let file = $("form, [type=file]");
-    const input = document.querySelector('.file');
-    let patternMail = /^[\w-\.]+@[\w-]+[\./]+[a-z]{2,4}$/g; 
-    let allowedExtensions = /(\.jpg|\.jpeg|\.png|\.docx|\.pdf|\.doc|\.rar|\.txt)$/i;
-    let patternPhone = /^\d{11}$/g;
-    let btnForm = $(".btnForm");
+    const validationMassegeTemplate = "Please! Enter a valid ",
+          emailValidationMassege = validationMassegeTemplate + "email address",
+          phoneValidationMassege = validationMassegeTemplate + "phone number",
+          fileValidationMassege = "Only file with the following extensions are allowed: jpg, jpeg, png, gif, doc, pdf, docx, txt, rar, zip";
+          fileSizeValidationMassege = "Sorry! Maximum upload file size: 25 MB. Your file size is:"
+    let email = $("form input[type=email]"),
+        phone = $("form input[type=tel]"),
+        invalidTextSize = document.querySelector(".invalidTextSize"),
+        invalidTextFile = document.querySelector(".invalidTextFile"),
+        phoneTextMassege = document.querySelector(".phoneTextMassege"),
+        emailTextMassege = document.querySelector(".emailTextMassege"),
+        patternMail = /^[\w-\.]+@[\w-]+[\./]+[a-z]{2,4}$/g,
+        allowedExtensions = /(\.jpg|\.jpeg|\.png|\.docx|\.pdf|\.doc|\.rar|\zip|\.txt)$/i,
+        patternPhone = /^\d{11}$/g,
+        btnCleanTextMassege = $(".btnForm");
 
-    ////Valid email
+    ////Validation email
     email.on("blur", function () {
         let email = $(this).val();
         if (email.length > 0 && (email.match(patternMail) || []).length !== 1) {
-            document.querySelector(".email").innerHTML = emailValidationMassege;
+            emailTextMassege.innerHTML = emailValidationMassege;
         }
     });
-    ////Valid phone number
+
+    ////Validation phone number
     phone.on("blur", function () {
         let phone = $(this).val();
         if (phone.length > 0 && (phone.match(patternPhone) || []).length !== 1)
-            document.querySelector(".phone").innerHTML = phoneValidationMassege;
+            phoneTextMassege.innerHTML = phoneValidationMassege;
     });
 
     ///Clear validationMassegeTemplate 
-    btnForm.on("click", function () {
+    btnCleanTextMassege.on("click", function () {
         let emailValue = email.val();
         let phoneValue = phone.val();
-           if (emailValue == 0 || (emailValue.match(patternMail)) || phoneValue == 0 || (phoneValue.match(patternPhone))) {
-            $("p.email, p.phone").remove();
+        if (emailValue == 0 || (emailValue.match(patternMail)) || phoneValue == 0 || (phoneValue.match(patternPhone))) {
+            phoneTextMassege.innerHTML = " ";
+            emailTextMassege.innerHTML = " ";
 
         }
     });
-
-    ////Valid file extensions
-
+    //Validation file extensions
     $(".file").on("change", function () {
         let customFileInputValue = $(this).val();
         if (!allowedExtensions.exec(customFileInputValue)) {
-            document.querySelector(".invalidTextMassege").innerHTML = fileValidationMassege;
-            customFileInputValue.value = '';
-            //  return false;
+            invalidTextFile.innerHTML = fileValidationMassege;
         } else if (allowedExtensions.exec(customFileInputValue)) {
-            $("p.invalidTextMassege").remove();
+            invalidTextFile.innerHTML = " ";
         }
-     });
- 
-       ////Valid file extensions 
-
-    input.addEventListener('change', e => {
-        const files = input.files;
-        let fileSize = 0;
-        let output = [];
-        let fileSizeSum;
-        let fileName;
-        for (let i = 0; i < files.length; i++) {
-            fileSize += files[i].size;
-            fileName = files[i].name;
-            let file = fileSize / 1024 / 1024;
-            fileSizeSum = file.toFixed(2);
-            output.push('<li>' + fileName + " " + '</li>');
-        }
-        if (fileSizeSum > 25) {
-            document.querySelector(".invalidTextSize").innerHTML = fileSizeValidationMassege + " " + fileSizeSum + " MB";
-        } else if (fileSizeSum < 25 || fileSizeSum == 25) {
-            $("p.invalidTextSize").remove();
-            return false;
-        } 
-        
-          document.querySelector('.listFiles').innerHTML = '<ul>' + output.join('')  + "Your file size is:" + " " + fileSizeSum + " " + "MB" + '</ul>';
-
-
-
     });
+    //Valiadtion file size
+    $(".file").on("change", function () {
+        let inputFile = document.querySelector('.file');
+        const files = inputFile.files;
+        let fileSize = 0;
+        let fileSizeSum;
+            for (let i = 0; i < files.length; i++) {
+            fileSize += files[i].size;
+           let file = fileSize / 1024 / 1024;
+            fileSizeSum = file.toFixed(2);
+        } if (fileSizeSum > 25) {
+            invalidTextSize.innerHTML = fileSizeValidationMassege + " " + fileSizeSum + " MB";
+        } else if (fileSizeSum <= 25) {
+            invalidTextSize.innerHTML = " ";
+        }
+ 
+});
 
 
 });
- 
