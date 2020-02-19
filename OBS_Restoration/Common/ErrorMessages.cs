b1780aa31626c7f4bbc72fb85504e32a7260e208
Inna <infarmationusa@gmail.com>
2020-02-19 11:16:31 +0200

    changed response message

--- OBS_Restoration/Common/ErrorMessages.cs
+++ OBS_Restoration/Common/ErrorMessages.cs
@@ -2,7 +2,7 @@
 {
     public static class ErrorMessages
     {
-        public const string SUCCESS_SENT_EMAIL_MESSAGE = "Email was send successfuly";
+        public const string SUCCESS_SENT_EMAIL_MESSAGE = "Your message has been sent successfully";
         public const string GENERAL_ERROR_MESSAGE = "An error has occurred during the processing of your request, please try again later.";
         public const string FAILED_VALIDATION_ERROR_MESSAGE = "Form validation is failed";
     }
