3a4eb5bd56d1f270370ff9265ca224d06a9d7a1e
Inna <infarmationusa@gmail.com>
2020-02-23 22:20:42 +0200

    project isotope

--- OBS_Restoration/OBS_Restoration/Content/Js/Custom/projects.js
+++ OBS_Restoration/OBS_Restoration/Content/Js/Custom/projects.js
@@ -5,23 +5,44 @@ $(function () {
         success: function (data) {
             if (data.Data && data.Success) {
                 let project = data.Data;
+                let img = project.Url;
+                console.log(project)
                 project.sort((a, b) => (a.Order > b.Order) ? 1 : (a.Order < b.Order) ? -1 : 1);
                 $.each(project, function (i, value) {
                     populateProjectsTab(value);
                     populateProjectsImg(value);
                 });
-                /////////////////////PROJECT ISOTOP
-                let $container = $(".project-container");
-                $container.isotope({
-                    itemSelector: '.project-container-img',
-                    filter: "*"
+                // init Isotope
+                var $grid = $(".grid").isotope({
+                    itemSelector: ".element-item",
+                    layoutMode: "fitRows"
                 });
-                $('.project-filter').on('click', 'a', function () {
-                    var filterValue = $(this).attr('data-filter');
-                    $container.isotope({ filter: filterValue });
-                    return false;
+
+                // filter functions
+                var filterFns = {
+                       filterProjects: function () {
+                        var filterValue = $(this).attr('data-filter');
+                        $container.isotope({ filter: filterValue });
+                        return false;
+                    },
+                        
+                };
+                // bind filter button click
+                $(".filters-button-group").on("click", "button", function () {
+                    var filterValue = $(this).attr("data-filter");
+                    // use filterFn if matches value
+                    filterValue = filterFns[filterValue] || filterValue;
+                    $grid.isotope({ filter: filterValue });
                 });
-            }
+                // change is-checked class on buttons
+                $(".button-group").each(function (i, buttonGroup) {
+                    var $buttonGroup = $(buttonGroup);
+                    $buttonGroup.on("click", "button", function () {
+                        $buttonGroup.find(".is-checked").removeClass("is-checked");
+                        $(this).addClass("is-checked");
+                    });
+                });
+             }
             else if (data.Data === null || data.Success === false) {
                 let errorProjectMessage = data.ErrorMessage;
                 document.querySelector('.errorProjectMessage').innerHTML = errorProjectMessage;
@@ -35,21 +56,21 @@ $(function () {
 function populateProjectsTab(value) {
     let templ;
     templ = document.getElementById('templateTab');
-    templ.content.querySelector(".tab").innerHTML = (value.Name);
-    templ.content.querySelector(".tab").setAttribute("data-filter", 'section[project="' + value.Name + '"]');
+    templ.content.querySelector(".button").innerHTML = (value.Name);
+    templ.content.querySelector(".button").setAttribute("data-filter", 'img[project="' + value.Name + '"]');
     var clon = templ.content.cloneNode(true);
-    document.querySelector(".project-filter").append(clon);
+    document.querySelector(".button-group").append(clon);
 }
 
 
 function populateProjectsImg(value) {
     let temp;
     temp = document.getElementById("templateImg");
-    temp.content.querySelector(".project-container-img").setAttribute("project", value.Name);
+    temp.content.querySelector(".element-item").setAttribute("project", value.Name);
     for (let i = 0, l = value.Images.length; i < l; i++) {
         let obj = value.Images[i];
         temp.content.querySelector('img').src = (obj.Url);
         var clon = temp.content.cloneNode(true);
-        document.querySelector(".project-container").append(clon);
+        document.querySelector(".grid").append(clon);
     }
 };
