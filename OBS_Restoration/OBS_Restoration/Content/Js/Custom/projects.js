7fff0baac759bba4f32a3d028dfd11af5618469d
Yevhen Toderash <yevhen.toderash@additiv.ch>
2020-02-24 08:50:57 +0200

    refactoring

--- OBS_Restoration/OBS_Restoration/Content/Js/Custom/projects.js
+++ OBS_Restoration/OBS_Restoration/Content/Js/Custom/projects.js
@@ -11,32 +11,20 @@ $(function () {
                     populateProjectsImg(value);
                 });
                 // init Isotope
-                var $grid = $(".grid").isotope({
+                var $grid = $(".images-group");
+                $grid.isotope({
                     itemSelector: ".element-item",
                     layoutMode: "fitRows"
                 });
-                // filter functions
-                var filterFns = {
-                    filterProjects: function () {
-                        var filterValue = $(this).attr('data-filter');
-                        $container.isotope({ filter: filterValue });
-                        return false;
-                    },
-                };
                 // bind filter button click
                 $(".filters-button-group").on("click", "button", function () {
                     var filterValue = $(this).attr("data-filter");
-                    // use filterFn if matches value
-                    filterValue = filterFns[filterValue] || filterValue;
                     $grid.isotope({ filter: filterValue });
                 });
                 // change is-checked class on buttons
-                $(".button-group").each(function (i, buttonGroup) {
-                    var $buttonGroup = $(buttonGroup);
-                    $buttonGroup.on("click", "button", function () {
-                        $buttonGroup.find(".is-checked").removeClass("is-checked");
-                        $(this).addClass("is-checked");
-                    });
+                $(".button-group button").click(function () {
+                    $('.button-group').find("button.is-checked").removeClass("is-checked");
+                    $(this).addClass("is-checked");
                 });
             }
             else if (data.Data === null || data.Success === false) {
@@ -50,21 +38,19 @@ $(function () {
 });
 
 function populateProjectsTab(value) {
-    let templ;
-    templ = document.getElementById('templateTab');
+    let templ = document.getElementById('tabTemplate');
     templ.content.querySelector(".button").innerHTML = (value.Name);
     templ.content.querySelector(".button").setAttribute("data-filter", 'img[project="' + value.Name + '"]');
     var clon = templ.content.cloneNode(true);
     document.querySelector(".button-group").append(clon);
 }
 function populateProjectsImg(value) {
-    let temp;
-    temp = document.getElementById("templateImg");
+    let temp = document.getElementById("imgTemplate");
     temp.content.querySelector(".element-item").setAttribute("project", value.Name);
     for (let i = 0, l = value.Images.length; i < l; i++) {
         let obj = value.Images[i];
         temp.content.querySelector('img').src = (obj.Url);
         var clon = temp.content.cloneNode(true);
-        document.querySelector(".grid").append(clon);
+        document.querySelector(".images-group").append(clon);
     }
 };
