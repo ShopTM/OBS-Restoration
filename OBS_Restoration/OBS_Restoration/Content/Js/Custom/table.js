5d8245f20aa4edd3ddfbb330c4de2ad159ef74e5
Inna <infarmationusa@gmail.com>
2020-03-05 23:14:52 +0200

    add fn

--- OBS_Restoration/OBS_Restoration/Content/Js/Custom/table.js
+++ OBS_Restoration/OBS_Restoration/Content/Js/Custom/table.js
@@ -1,64 +1,56 @@
-﻿$(".nav-tabs a").click(function () {
-    $(this).tab('show');
-});
-// Add the name of the file appear on select
+﻿// Add the name of the file appear on select
 $(".custom-file-input").on("change", function () {
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
-$(function () {
+function populateTableServices() {
     $.ajax({
-        url: '/home/getServices',
+        url: '/Admin/getServices',
         type: 'GET',
         success: function (data) {
             if (data.Data && data.Success) {
                 let services = data.Data;
-                $.each(services, function (i, value) {
-                    populateTableServices(value)
-               
-
+                 $.each(services, function (i, service) {
+                    populateServiceRow(service)
                 });
             }
         }
     });
     $("[data-toggle=tooltip]").tooltip();
+}
+$(function () {
+    populateTableServices();
 });
 
-function populateTableServices(value) {
-    let templ, td, tbody, clone;
-    templ = document.querySelector("template");
-    td = templ.content.querySelectorAll("td");
-    td[1].textContent = value.Name;
-    td[2].textContent = value.Description;
-    td[3].querySelector('img').src = value.ImgUrl;
-    tbody = document.getElementsByTagName("tbody");
-    clone = document.importNode(templ.content, true);
+
+let templ = document.querySelector("template");
+let td = templ.content.querySelectorAll("td");
+let tbody = document.getElementsByTagName("tbody");
+let urlImg = "../../Content/Images/Services/";
+
+function populateServiceRow(service) {
+    td[1].textContent = service.Name;
+    td[2].textContent = service.Description;
+    td[3].querySelector('img').src = urlImg + service.ImgUrl;
+    let clone = document.importNode(templ.content, true);
     tbody[0].appendChild(clone);
 }
 
-let edit = document.querySelector('button');
 
 
-////Open modal window by edit btn
-
-function showModal(value) {
-    $('#tableModal').modal('show');
-}
-
-////get value form
-
-let namSservices = $(".modal-body input").val();
-let descriptionServices = $(".modal-body textarea").val();
-let imageServices = $('input[type=file]').val();
-
-// Edit row on edit button click
-function getCurrentElementEdit() {
-  $('tr')
-
-}
-
-
-edit.addEventListener("click", showModal); 
-edit.addEventListener("click", getCurrentElementEdit);
-
+//Add Data Function  
+$('.add').on('click', function (form) {
+    var serializeFormData = $('form').serialize();
+    console.log(serializeFormData)
+    $.ajax({
+        data: serializeFormData,
+        processData: false,
+        contentType: false,
+         success: function (result) {
+            console.log(result)
+            
+        }
+    });
+  
+})
 
