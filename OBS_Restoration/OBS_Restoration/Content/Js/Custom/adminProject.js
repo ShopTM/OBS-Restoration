62a807cf1f66784f10a1872fd1026bc6a55177b8
Inna <infarmationusa@gmail.com>
2020-03-20 22:33:49 +0200

    added func populateTableProjectsImg

--- OBS_Restoration/OBS_Restoration/Content/Js/Custom/adminProject.js
+++ OBS_Restoration/OBS_Restoration/Content/Js/Custom/adminProject.js
@@ -12,9 +12,9 @@ function populateTableProjects() {
             if (response.Data && response.Success) {
                 let projects = response.Data;
                 $.each(projects, function (i, projects) {
-                    populateProjectseRow(projects)
-                    populateProjectsImg(projects) 
-          
+                    populateTableProjectsImg(projects)
+                    populateProjectseRow(projects) 
+                   
                 });
             }
         }
@@ -25,28 +25,36 @@ function populateTableProjects() {
 $(function () {
     populateTableProjects();
 });
-let templProj = document.querySelector("#tableProjectsTemplate");
-let tdProj = templProj.content.querySelectorAll("td");
-let tbodyProj = document.querySelector(".tableProjectsBody");
-let urlImgProj = "../../Content/Images/Projects/";
-let tdImg = templProj.content.querySelector("td")[4];
- 
+let templProject = document.querySelector("#tableProjectsTemplate");
+let tdProject = templProject.content.querySelectorAll("td");
+let tbodyProject = document.querySelector(".tableProjectsBody");
+let urlImgProject = "../../Content/Images/Projects/";
+
 function populateProjectseRow(projects) {
-    tdProj[3].textContent = projects.Name;
-    cloneProj = document.importNode(templProj.content, true);
-    tbodyProj.appendChild(cloneProj);
+    tdProject[3].textContent = projects.Name;
+    templProject.content.querySelector('.td-img').setAttribute('projectId', projects.Id);
+    cloneProject = document.importNode(templProject.content, true);
+    tbodyProject.appendChild(cloneProject);
+    
 }
 
+function populateTableProjectsImg(projects) {
+    let tdImages = templProject.content.querySelector('.td-img');
+    let projectId = tdImages.getAttribute('projectId')
+   for (let i = 0, l = projects.Images.length; i < l; i++) {
+       projectsImg = projects.Images[i];
+       if (projectsImg.projectId)
 
-function populateProjectsImg(projects) {
-    templProj.content.querySelector(".img-proj").setAttribute("projectId", projects.projectId);
-        for (let i = 0, l = projects.Images.length; i < l; i++) {
-            let projectsImg = projects.Images[i];
-            templProj.content.querySelector('img').src = (projectsImg.Url);
-            var clon = templProj.content.cloneNode(true);
-            tdImg.appendChild(clon);
 
-          
-        }
+               $('.td-img').append('<img src="" alt="" class="img-proj" />')
+               document.querySelector('.img-proj').src = urlImgProject + projectsImg.Url;
+              
+      
+      
+
+   }
     }
+ 
+
+
 
